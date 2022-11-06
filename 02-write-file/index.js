248af8eec63273d7e6bed73307007dea38641808
unknown <sedricgreen6@gmail.com>
2022-11-06 23:35:57 +0200

    fix: fix second task

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,27 +1,32 @@
 const fs = require("fs");
 
 fs.writeFile("02-write-file/hello.txt", "", function (error) {
-
   if (error) throw error;
 });
 
-const readline = require('readline');
-const { stdin: input, stdout: output } = require('process');
+const readline = require("readline");
+const { stdin: input, stdout: output } = require("process");
+// const process = require("process");
 
 const rl = readline.createInterface({ input, output });
 
-rl.question('Hello!\n', (answer) => {
+rl.question("Hello!\n", (answer) => {
   fs.appendFile("02-write-file/hello.txt", answer, function (error) {
     if (error) throw error;
-  })
-
-  rl.on('line', (input) => {
-    fs.appendFile("02-write-file/hello.txt", ` ${input}`, function (error) {
-      if (error) throw error;
-    })
-    if (input == 'exit') {
-      console.log('GoodBye!');
+    if (answer === "exit") {
+      console.log("GoodBye!");
       rl.close();
     }
   });
+
+  rl.on("line", (input) => {
+    fs.appendFile("02-write-file/hello.txt", ` ${input}`, function (error) {
+      if (error) throw error;
+    });
+  });
+});
+
+rl.on("SIGINT", () => {
+  console.log("GoodBye!");
+  rl.close();
 });
